eeb5b26b10389e23ad235cd7ae6f0052beae8d55
Christian Nielsen <Christian Nielsen>
2017-01-08 16:21:57 +1000

    Added set-servo.py

---
+++ set-servo.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 08 15:49:15 2017
+
+@author: Christian
+"""
+
+import math
+import sys
+
+pwmpaths = ["/sys/class/pwm/pwmchip0/pwm0",
+	   "/sys/class/pwm/pwmchip0/pwm1"]
+    
+adcpaths = ["/sys/bus/iio/devices/iio:device0/in_voltage0_raw"]
+
+#pwmpaths = ["c:/temp/pwm0",
+#	   "c:/temp/pwm1"]
+#    
+#adcpaths = ["c:/temp/in_voltage0_raw"]
+
+def clamp(minx, maxx, x):
+    if (x < minx):
+        return minx
+    elif (x > maxx):
+        return maxx
+    else:
+        return x
+
+def calculateduty(v):
+    duty = int((v*4.0 / math.pi) * 600000 + 1500000)
+    return clamp(900000, 2100000, duty)
+
+def setvalue(path, value):
+    f = open(path,"w")   
+    f.write(str(value))
+   
+def readvalue(path):
+    f = open(path,"r")   
+    return int(f.readline())
+    
+def setperiod(path, period):
+    setvalue(path+"/period", period)    
+    
+def setenable(path):
+    setvalue(path+"/enable", 1)
+    
+def setdutycycle(path, dutycycle):
+    setvalue(path+"/duty_cycle", dutycycle)
+
+def setangle(pwmpath, angle):
+    duty = calculateduty(angle)
+    setdutycycle(pwmpath, duty)
+
+def readvoltage(adcpath):
+    return readvalue()
+    
+servo_index = int(sys.argv[1])
+angle = float(sys.argv[2])
+
+pwmpath = pwmpaths[servo_index]
+adcpath = adcpaths[servo_index]
+
+setperiod(pwmpath, 20000000)
+setangle(pwmpath, math.radians(angle))
+setenable(pwmpath)
+
+for i in range(10):
+    print readvalue(adcpath)
+    
